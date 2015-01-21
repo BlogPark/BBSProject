12c9403de6e153c18387bf15a9edd785188a4678
zhangchenghang <chenghang1937@126.com>
2015-01-21 16:15:56 +0800

    筹备后台管理首页

---
+++ BBSProject.WebUI/Areas/Admin/Controllers/OperateController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BBSProject.WebUI.Areas.Admin.Controllers
+{
+    public class OperateController : Controller
+    {
+        //
+        // GET: /Admin/Operate/
+
+        public ActionResult Index()
+        {
+            ViewBag.Title = "贴吧后台管理系统";
+            ViewBag.Message = "此处为后台管理系统首页位置";
+            return View();
+        }
+
+    }
+}
---
+++ BBSProject.WebUI/Controllers/PostsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BBSProject.WebUI.Controllers
+{
+    public class PostsController : Controller
+    {
+        //
+        // GET: /Posts/
+
+        public ActionResult Index()
+        {
+            ViewBag.Title = "贴吧首页";
+            ViewBag.Message = "这是一个新的开始，一定要坚持做完";
+            return View();
+        }
+
+    }
+}
