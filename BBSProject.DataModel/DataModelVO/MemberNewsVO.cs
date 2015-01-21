eaa91e904d79df9be438e476d468156836ce4060
zhangchenghang <chenghang1937@126.com>
2015-01-21 14:17:01 +0800

    Dapper学习处理

---
+++ BBSProject.DataModel/DataModelVO/MemberNewsVO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBSProject.DataModel.Model
+{
+    public class MemberNewsVO : MemberNews
+    {
+        public MemberNewsDetail details { get; set; }
+    }
+}
