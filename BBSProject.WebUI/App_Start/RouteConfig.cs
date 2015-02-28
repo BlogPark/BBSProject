810239652677a9b0705f9c06e996f15a02f43a67
zhangchenghang <chenghang1937@126.com>
2015-02-28 17:35:10 +0800

    q

--- BBSProject.WebUI/App_Start/RouteConfig.cs
+++ BBSProject.WebUI/App_Start/RouteConfig.cs
@@ -13,6 +13,7 @@ namespace BBSProject.WebUI
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
